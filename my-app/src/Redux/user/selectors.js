4fe8589727748f4934eacd039653449f2bfde526
SerhiiSamara <ndaair@gmail.com>
2023-05-01 16:42:58 +0300

    1

---
+++ my-app/src/Redux/user/selectors.js
@@ -0,0 +1,5 @@
+export const selectUsers = state => state.user.items;
+// export const selectFilter = state => state.filter;
+export const selectIsLoading = state => state.user.isLoading;
+export const selectError = state => state.user.error;
+export const selectPage = state => state.user.page;
